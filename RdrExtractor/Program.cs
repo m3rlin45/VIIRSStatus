a45986c2ae4393ed63363d31be6273a3aa2c591a
Chris Dewan <Chris.Dewan@outlook.com>
2014-06-18 23:18:47 -0700

    Added the RdrExtractor Project and tested that it can startup the HD5.net library on an azure web job

---
+++ RdrExtractor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HDF5DotNet;
+
+namespace RdrExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            H5.Open();
+            Console.WriteLine("HDF5 Verion: {0}.{1}.{2}", H5.Version.Major, H5.Version.Minor, H5.Version.Release);
+            H5.Close();
+        }
+    }
+}
